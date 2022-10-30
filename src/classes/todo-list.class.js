ab84b3a75422f32787cb122ff969a7e6410a5c50
Edil Soto <edil.soto@hunter.do>
2022-10-29 22:25:38 -0400

    Agrega funcionalidad local storage

--- src/classes/todo-list.class.js
+++ src/classes/todo-list.class.js
@@ -8,21 +8,24 @@ export class TodoList {
     nuevoTodo(todo) {
 
         this.todos.push(todo);
+        this.guardarLocalStorage();
+
     }
 
     eliminarTodo(id) {
 
-        this.todos = this.todos.filter( todo => todo.id != id )
-
+        this.todos = this.todos.filter(todo => todo.id != id);
+        this.guardarLocalStorage();
     }
 
     marcarCompletado(id) {
-        
+
         for (const todo of this.todos) {
 
             if (todo.id == id) {
 
                 todo.completado = !todo.completado;
+                this.guardarLocalStorage();
                 break;
 
             }
@@ -31,6 +34,21 @@ export class TodoList {
 
     eliminarCompletados() {
 
-        this.todos = this.todos.filter( todo => !todo.completado)
+        this.todos = this.todos.filter(todo => !todo.completado);
+        this.guardarLocalStorage();
+
+
+    }
+
+    guardarLocalStorage() {
+
+        localStorage.setItem('todo', JSON.stringify(this.todos));
+
+    }
+
+    cargarLocalStorage() {
+
+
+
     }
 }
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,8 +1,9 @@
-import { Todo} from "../classes";
+import { Todo } from "../classes";
 import { todoList } from "../index";
 
 const divTodoList = document.querySelector('.todo-list');
 const txtInput = document.querySelector('.new-todo');
+const btnBorrar = document.querySelector('.clear-completed');
 
 export const crearTodoHtml = (todo) => {
 
@@ -16,41 +17,54 @@ export const crearTodoHtml = (todo) => {
     <input class="edit" value="Create a TodoMVC template">
 </li>`
 
-const div = document.createElement('div');
-div.innerHTML = htmlTodo;
+    const div = document.createElement('div');
+    div.innerHTML = htmlTodo;
 
-divTodoList.prepend(div.firstElementChild);
+    divTodoList.prepend(div.firstElementChild);
 
-return div;
+    return div;
 }
 
 txtInput.addEventListener('keyup', (event) => {
-    
-    if(event.keyCode === 13 && txtInput.value.length > 0){
 
-        const nuevoTodo = new Todo( txtInput.value);
-        todoList.nuevoTodo( nuevoTodo);
+    if (event.keyCode === 13 && txtInput.value.length > 0) {
+
+        const nuevoTodo = new Todo(txtInput.value);
+        todoList.nuevoTodo(nuevoTodo);
         crearTodoHtml(nuevoTodo);
         txtInput.value = '';
-        
     }
 })
 
-divTodoList.addEventListener('click', (event) =>{
-  
+divTodoList.addEventListener('click', (event) => {
+
     const nombreElemento = event.target.localName;
     const todoElemento = event.target.parentElement.parentElement;
     const todoId = todoElemento.getAttribute('data-id');
-    
-    if (nombreElemento.includes('input')){
+
+    if (nombreElemento.includes('input')) {
 
         todoList.marcarCompletado(todoId);
         todoElemento.classList.toggle('completed');
-        
-    }else if (nombreElemento.includes('button')){
+
+    } else if (nombreElemento.includes('button')) {
 
         todoList.eliminarTodo(todoId);
-        divTodoList.removeChild( todoElemento)
+        divTodoList.removeChild(todoElemento)
     }
-      
 })
+
+btnBorrar.addEventListener('click', () => {
+
+    todoList.eliminarCompletados();
+
+    for (let i = divTodoList.children.length - 1; i >= 0; i--) {
+
+        const elemento = divTodoList.children[i];
+
+        if (elemento.classList.contains('completed')) {
+            
+            divTodoList.removeChild(elemento);
+        }
+    }
+}) 
